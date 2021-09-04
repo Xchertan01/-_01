3ec10894b140861ef0f2dbe3195f83de75a5dd7b
Xchertan01 <Xchertan@outlook.com>
2021-09-04 09:49:57 +0800

    使用scatter()绘制散点图并设置样式

---
+++ atter_squares.py
@@ -0,0 +1,26 @@
+# -*- coding = utf-8 -*-
+# @Software: Pycharm
+
+import matplotlib.pyplot as plt
+
+plt.style.use("seaborn")
+fig, ax = plt.subplots()
+ax.scatter(2, 4, s=200)
+
+# 设置图表标题并给坐标轴加上标签
+ax.set_title("square numbers", fontsize=24)
+ax.set_xlabel("number", fontsize=14)
+ax.set_ylabel("square", fontsize=14)
+
+#设置刻度标记的大小
+ax.tick_params(axis="both", which="major", labelsize=14)
+
+plt.show()
+
+
+
+
+
+
+
+
